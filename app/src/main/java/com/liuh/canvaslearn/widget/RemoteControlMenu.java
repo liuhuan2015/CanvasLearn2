4d3264a409f503b90c6c33f685281d6f249dc3f4
liuh <2210712252@qq.com>
2018-04-11 10:22:48 +0800

    完成遥控器上下左右界面控件,可点击不同按键

--- app/src/main/java/com/liuh/canvaslearn/widget/RemoteControlMenu.java
+++ app/src/main/java/com/liuh/canvaslearn/widget/RemoteControlMenu.java
@@ -10,13 +10,47 @@ import android.graphics.RectF;
 import android.graphics.Region;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.gcssloop.view.utils.CanvasAidUtils;
 
 /**
  * Date: 2018/4/10 11:37
- * Description:
+ * Description:遥控器的上下左右界面
+ * <p>
+ * 一.绘制上下左右键,其中的角度值是需要计算下的
+ * right_p.addArc(bigCircle, -42, bigSweepAngle);
+ * right_p.arcTo(smallCircle, 40, smallSweepAngle);
+ * right_p.close();
+ * right.setPath(right_p, globalRegion);
+ * <p>
+ * 二.判断区域,使用了Region类的contains方法
+ * if (center.contains(x, y)) {
+ * return CENTER;
+ * } else if (right.contains(x, y)) {
+ * return RIGHT;
+ * } else if (down.contains(x, y)) {
+ * return DOWN;
+ * } else if (left.contains(x, y)) {
+ * return LEFT;
+ * } else if (up.contains(x, y)) {
+ * return UP;
+ * }
+ * return -1;
+ * <p>
+ * 三.因为画布坐标系和屏幕原始坐标系不一致,这里也做了手指触摸点的坐标位置转换
+ * onTouchEvent方法中:
+ * float[] pts = new float[2];
+ * pts[0] = event.getRawX();
+ * pts[1] = event.getRawY();
+ * mMapMatrix.mapPoints(pts);
+ * <p>
+ * onDraw方法中
+ * //获取测量矩阵(逆矩阵)
+ * if (mMapMatrix.isIdentity()) {
+ * canvas.getMatrix().invert(mMapMatrix);
+ * }
  */
 
 public class RemoteControlMenu extends View {
@@ -37,10 +71,14 @@ public class RemoteControlMenu extends View {
     int touchFlag = -1;
     int currentFlag = -1;
 
-//    MenuListener mListener = null;
+    MenuListener mListener = null;
 
     int mDefaultColor = 0xFF4E5268;
     int mTouchedColor = 0xFFDF9C81;
+
+    public void setmListener(MenuListener mListener) {
+        this.mListener = mListener;
+    }
 
     public RemoteControlMenu(Context context) {
         this(context, null);
@@ -87,39 +125,102 @@ public class RemoteControlMenu extends View {
         RectF smallCircle = new RectF(-sr, -sr, sr, sr);
 
         float bigSweepAngle = 84;
-        float smallSweepAngle = -84;
+        float smallSweepAngle = -80;
 
         //根据视图大小，初始化Path和Region
         center_p.addCircle(0, 0, 0.2f * minWidth, Path.Direction.CW);
         center.setPath(center_p, globalRegion);
 
         right_p.addArc(bigCircle, -42, bigSweepAngle);
-        right_p.arcTo(smallCircle, 42, smallSweepAngle);
+        right_p.arcTo(smallCircle, 40, smallSweepAngle);
         right_p.close();
         right.setPath(right_p, globalRegion);
 
         down_p.addArc(bigCircle, 48, bigSweepAngle);
-        down_p.arcTo(smallCircle, 132, smallSweepAngle);
+        down_p.arcTo(smallCircle, 130, smallSweepAngle);
         down_p.close();
         down.setPath(down_p, globalRegion);
 
         left_p.addArc(bigCircle, 138, bigSweepAngle);
-        left_p.arcTo(smallCircle, 222, smallSweepAngle);
+        left_p.arcTo(smallCircle, 220, smallSweepAngle);
         left_p.close();
         left.setPath(left_p, globalRegion);
 
         up_p.addArc(bigCircle, 228, bigSweepAngle);
-        up_p.arcTo(smallCircle, 312, smallSweepAngle);
+        up_p.arcTo(smallCircle, 310, smallSweepAngle);
         up_p.close();
         up.setPath(up_p, globalRegion);
+    }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float[] pts = new float[2];
+        pts[0] = event.getRawX();
+        pts[1] = event.getRawY();
+        mMapMatrix.mapPoints(pts);
 
+        int x = (int) pts[0];
+        int y = (int) pts[1];
+
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                touchFlag = getTouchedPath(x, y);
+                currentFlag = touchFlag;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                currentFlag = getTouchedPath(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                currentFlag = getTouchedPath(x, y);
+                //如果手指按下区域和抬起区域相同且不为空,则为点击事件
+                if (currentFlag == touchFlag && currentFlag != -1 && mListener != null) {
+                    if (currentFlag == CENTER) {
+                        mListener.onCenterClicked();
+                    } else if (currentFlag == RIGHT) {
+                        mListener.onRightClicked();
+                    } else if (currentFlag == DOWN) {
+                        mListener.onDownClicked();
+                    } else if (currentFlag == LEFT) {
+                        mListener.onLeftClicked();
+                    } else if (currentFlag == UP) {
+                        mListener.onUpClicked();
+                    }
+                }
+                touchFlag = currentFlag = -1;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                touchFlag = currentFlag = -1;
+                break;
+        }
+        invalidate();
+        return true;
+    }
+
+    //获取当前触摸点在哪个区域
+    private int getTouchedPath(int x, int y) {
+        if (center.contains(x, y)) {
+            return CENTER;
+        } else if (right.contains(x, y)) {
+            return RIGHT;
+        } else if (down.contains(x, y)) {
+            return DOWN;
+        } else if (left.contains(x, y)) {
+            return LEFT;
+        } else if (up.contains(x, y)) {
+            return UP;
+        }
+        return -1;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.translate(mWidth / 2, mHeight / 2);
+
+        //获取测量矩阵(逆矩阵)
+        if (mMapMatrix.isIdentity()) {
+            canvas.getMatrix().invert(mMapMatrix);
+        }
 
         CanvasAidUtils.set2DAxisLength(500, 500, 700, 500);
         CanvasAidUtils.setLineColor(Color.CYAN);
@@ -131,5 +232,35 @@ public class RemoteControlMenu extends View {
         canvas.drawPath(left_p, mPaint);
         canvas.drawPath(up_p, mPaint);
 
+        //绘制触摸区域颜色
+        mPaint.setColor(mTouchedColor);
+        if (currentFlag == CENTER) {
+            canvas.drawPath(center_p, mPaint);
+        } else if (currentFlag == RIGHT) {
+            canvas.drawPath(right_p, mPaint);
+        } else if (currentFlag == DOWN) {
+            canvas.drawPath(down_p, mPaint);
+        } else if (currentFlag == LEFT) {
+            canvas.drawPath(left_p, mPaint);
+        } else if (currentFlag == UP) {
+            canvas.drawPath(up_p, mPaint);
+        }
+        mPaint.setColor(mDefaultColor);
+
     }
+
+    //点击事件监听器
+    public interface MenuListener {
+
+        void onCenterClicked();
+
+        void onRightClicked();
+
+        void onDownClicked();
+
+        void onLeftClicked();
+
+        void onUpClicked();
+    }
+
 }
